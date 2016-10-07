e62afb7cce6297a9c42123ade3debbd27c09a1f7
Shadi Jam <shadijam00@gmail.com>
2016-10-07 12:19:38 -0500

    random function add

--- BlackJack_sj/Cards.cs
+++ BlackJack_sj/Cards.cs
@@ -12,19 +12,19 @@ namespace BlackJack
 
     public enum Rank
     {
-        Ace,
-        Deuce,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Eight,
-        Nine,
-        Ten,
-        Jack,
-        Queen,
-        King
+        Ace = 11,
+        Deuce = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 10,
+        Queen = 10,
+        King = 10
     }
 
     public class Card
@@ -61,10 +61,13 @@ namespace BlackJack
         {
             return new Card[0];
         }
+
+   //     dealerDeck.getRandomized();
+   //think about using the "Swap" function instead of getRandomized after random card pick choice
     }
 }
 
-
+//creates random card pick function
 public class Program
 {
     static int random()
@@ -78,3 +81,44 @@ public class Program
 
     }
 }
+/*creates shuffle function
+public Card[] Cards;
+public void Shuffle()
+{
+    Card[] currentDeck = this.Cards;
+    this.Cards = new Card[currentDeck.Length];
+    List<int> freeIndices = new List<int>(currentDeck.Length);
+    Random rand = new Random();
+
+    for (int idx = 0; idx < currentDeck.Length; idx++)
+        freeIndices.Add(idx);
+
+    foreach (Card card in currentDeck)
+    {
+
+        int indexOfNewIdx = rand.Next(freeIndices.Count);
+        int newIdxOfCard = freeIndices[indexOfNewIdx];
+
+        this.Cards[newIdxOfCard] = card;
+        freeIndices.Remove(newIdxOfCard);
+    }
+}
+/*creates new deck 
+static void Main(string[] args)
+{
+    Deck dealerDeck = new Deck();
+    Deck shuffledDeck = new Deck();
+
+    for (int i = 0; i < dealerDeck.cards.Length; i++)
+    {
+        Console.Write("Card(0): ", i);
+        Console.WriteLine("Suit: (0) ", dealerDeck.cards[i].suit.ToString());
+        Console.Write("Card (0): ", i);
+        Console.WriteLine("Rank: (0) ", dealerDeck.cards[i].rank.ToString());
+        //can combine all the above into one line of code//
+    }
+}
+// 							  
+
+
+
